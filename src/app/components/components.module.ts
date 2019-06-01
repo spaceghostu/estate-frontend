0f9dc63960f32304855ce0c54d3b9ba07f430528
Calvin Durrant <calvindurrant@gmail.com>
2019-06-01 11:47:57 +0200

    added sidebar component

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -1,8 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { SidebarComponent } from '../sidebar/sidebar.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [SidebarComponent],
   imports: [
     CommonModule
   ]
