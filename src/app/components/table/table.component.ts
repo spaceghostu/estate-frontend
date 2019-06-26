ecc9944b7621fd6cc1ffdcc4988ac5def3858b9b
Calvin Durrant <calvindurrant@gmail.com>
2019-06-26 15:09:10 +0200

    Added filter to table

--- src/app/components/table/table.component.ts
+++ src/app/components/table/table.component.ts
@@ -1,39 +1,49 @@
-import { Component, ViewChild, AfterViewChecked } from '@angular/core';
+import { Component, ViewChild, AfterContentInit } from '@angular/core';
 import { MatPaginator, MatSort, MatTableDataSource } from '@angular/material';
 import { EstateState } from 'src/app/estates/+state/estate.reducer';
 import { Store } from '@ngrx/store';
 import { estateQuery } from '../../estates/+state/estate.selectors';
 import { Entity } from '../../estates/+state/estate.reducer';
 import { LoadEstate } from '../../estates/+state/estate.actions';
-import { Observable, Subscription } from 'rxjs';
-import { TableDataSource } from './table-datasource';
+import { Observable } from 'rxjs';
+import { filter } from 'bluebird';
 
 @Component({
   selector: 'es-table',
   templateUrl: './table.component.html',
   styleUrls: ['./table.component.scss']
 })
-export class TableComponent implements AfterViewChecked {
+export class TableComponent implements AfterContentInit {
   @ViewChild(MatPaginator) paginator: MatPaginator;
   @ViewChild(MatSort) sort: MatSort;
   loaded$: Observable<boolean>;
   displayedColumns = ['name', 'id', 'estate-no', 'will', 'will-date', 'actions'];
-  dataSource: TableDataSource<Entity>;
-  dataSubscription: Subscription;
+  dataSource: MatTableDataSource<Entity>;
+  filterValue = '';
 
   constructor(private store: Store<EstateState>) {
     this.loaded$ = this.store.select(estateQuery.getLoaded);
   }
 
-  ngAfterViewChecked() {
+  ngAfterContentInit() {
+    this.store.dispatch(new LoadEstate());
     this.loaded$.subscribe((loaded: boolean) => {
       if (loaded) {
-        this.store.select(estateQuery.getAllEstate).subscribe(data => {
-          this.dataSource = new TableDataSource<Entity>(data, this.paginator, this.sort);
+        this.store.select(estateQuery.getAllEstate).subscribe((data: Entity[]) => {
+          this.dataSource = new MatTableDataSource<Entity>(data);
+          this.dataSource.paginator = this.paginator;
+          this.dataSource.sort = this.sort;
         });
-      } else {
-        this.store.dispatch(new LoadEstate());
       }
     });
+  }
+
+  applyFilter(filterValue: string) {
+    this.filterValue = filterValue;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+
+    if (this.dataSource.paginator) {
+      this.dataSource.paginator.firstPage();
+    }
   }
 }
